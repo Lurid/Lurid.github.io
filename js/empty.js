3460d89a6e635e4e9b8ca3beef3b2b6ae7ea5794
Sasha <epticid@gmail.com>
2023-04-04 11:17:08 +0600

    c

--- js/empty.js
+++ js/empty.js
@@ -1,8 +1,10 @@
-var requestURL2, request2, response2, lang2;
+var requestURL2 = 'lang/ru.json', request2, response2, lang2;
 async function qwerf2() {
-    requestURL2 = "/lang/ru.json";
     request2 = new Request(requestURL2);
-
     response2 = await fetch(request2);
     lang2 = await response2.json();
+    console.log(lang2);
 }
+window.addEventListener("DOMContentLoaded", (event) => {
+    qwerf2();
+});
